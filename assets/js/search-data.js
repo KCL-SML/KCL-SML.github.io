c55429b561973ae22110276f4ed572320ba4bddb
li-yp <yunpeng.li@kcl.ac.uk>
2026-07-20 06:01:20 +0000

    Deploying to gh-pages from @ TAIR-lab/TAIR-lab.github.io@72980846c394f1f8cdd62cadcbff034d98c21f0f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -546,7 +546,7 @@ ninja.data = [{
           section: "News",},{id: "news-becertain-a-king-s-and-surrey-spin-out-has-completed-a-1-7-million-pre-seed-round-led-by-sure-valley-ventures-with-support-from-innovate-uk-to-bring-clinical-grade-ai-to-dental-diagnostics-read-the-king-s-news-story",
           title: 'BeCertain, a King’s and Surrey spin-out, has completed a £1.7 million pre-seed round,...',
           description: "",
-          section: "News",},{id: "news-we-are-recruiting-a-four-year-postdoctoral-research-associate-to-join-the-translational-ai-research-lab-working-on-trustworthy-ai-for-scientific-discovery-and-biomanufacturing-as-part-of-a-horizon-europe-project-the-deadline-is-9-august-2026-see-the-openings-page",
+          section: "News",},{id: "news-we-are-recruiting-a-four-year-postdoctoral-research-associate-to-join-the-translational-ai-research-lab-working-on-trustworthy-ai-for-scientific-discovery-and-biomanufacturing-as-part-of-a-horizon-europe-project-the-deadline-for-applications-is-9-august-2026-see-the-openings-page",
           title: 'We are recruiting a four-year Postdoctoral Research Associate to join the Translational AI...',
           description: "",
           section: "News",},{id: "projects-bayesian-detector-combination",
